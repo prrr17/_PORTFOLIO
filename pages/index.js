34ca3b73a196aed4340d0d1bf834e6c634fa7f5f
Paula <rdb.paula@gmail.com>
2022-11-20 10:15:47 +0700

    more css

--- pages/index.js
+++ pages/index.js
@@ -35,18 +35,18 @@ export default function Home() {
             <div className='col-md-6'>
               <div className={`${utilStyles.uxBox}`}>
                 {' '}
-                <h3
-                  className={`${utilStyles.headingXl} text-md-end me-5`}
+                <h4
+                  className={`${utilStyles.headingLg} text-md-end me-5`}
                   id='uxWord'
                   alt='ux'
                 >
                   UX
-                </h3>
+                </h4>
               </div>{' '}
             </div>
           </div>
-          <div className='row g-0'>
-            <div className='col-md-6 col-sm-12'>
+          <div className='mx-auto row g-0'>
+            <div className='col-lg-6 col-md-12'>
               {' '}
               <h1
                 className={`ps-1 text-md-end ${utilStyles.heading3Xl} ${utilStyles}`}
@@ -55,7 +55,7 @@ export default function Home() {
               </h1>
               <hr className={`mx-auto ${utilStyles.underlineVD}`}></hr>
             </div>
-            <div className='col-md-6 col-sm-12'>
+            <div className='col-lg-6 col-md-12'>
               <div
                 class={`${utilStyles.arrowDown} ${utilStyles.lineVertical}`}
               ></div>
@@ -70,7 +70,7 @@ export default function Home() {
           <div className='row pb-4'>
             <div className={utilStyles.findOutBox}>
               <p
-                className={`mt-n5 animate__animated animate__fadeInDown animate__delay-1.5s ${utilStyles.verticalText}`}
+                className={`animate__animated animate__fadeInDown animate__delay-1.5s ${utilStyles.verticalText}`}
               >
                 find out more
               </p>{' '}
@@ -221,11 +221,11 @@ export default function Home() {
                 </div>
               </div>
               <div className='col-sm-7 border-primary py-4 ps-4' id='statement'>
-                <p className={`d-flex ${utilStyles.textDescriptions}`}>Hey there,</p>
-                <p className={`d-flex ${utilStyles.textDescriptions}`}>
+                <p className={` ${utilStyles.textDescriptions}`}>Hey there,</p>
+                <p className={` ${utilStyles.textDescriptions}`}>
                   Happy to see you here :){' '}
                 </p>
-                <p className={`d-flex ${utilStyles.textDescriptions}`}>
+                <p className={` ${utilStyles.textDescriptions}`}>
                   My name is Paula and, at uni, I studied sciences (of the sea)
                   and then English. Somewhat later on, I realized that I was
                   missing some creative expression to fill my days. Since I was
@@ -239,18 +239,35 @@ export default function Home() {
                   which inspired and motivated me a to make a career change,
                   pivoting towards these fields.
                 </p>
-                <p className={`d-flex ${utilStyles.textDescriptions}`}>
+                <p className={`${utilStyles.textDescriptions}`}>
                   On a more personal side, I am quite Aristotelian in looking
-                  for the "Golden Mean" in life —you can't spend the day in
-                  front of a screen (can you?), so my other usual activities
-                  often involve socializing with close people enjoying a
-                  beer/coffee and a conversation, walking through cities and
-                  nature, learning something new, reading (almost) everything
-                  that falls into my hands, eat and cook yummy healthy food, the
-                  sea, yoga, and exercising.{' '}
+                  for the "
+                  <a
+                    className={`fw-bold text-decoration-none ${utilStyles.textDescriptions}`}
+                    href='https://en.wikipedia.org/wiki/Golden_mean_(philosophy)'
+                    alt='"golden Mean" on Wikipedia'
+                    target='_blank'
+                  >
+                    Golden Mean
+                  </a>
+                  " in life —you can't spend the day in front of a screen (can
+                  you?), so my other usual activities often involve socializing
+                  with close people enjoying a beer/coffee and a conversation,
+                  walking through cities and nature, learning something new,
+                  reading (almost) everything that falls into my hands, eat and
+                  cook yummy healthy food, the sea, yoga, and exercising.{' '}
                 </p>
-                <p className={`d-flex ${utilStyles.textDescriptions}`}>
-                  Let's create something meaningful (and beautiful) together 🌻
+                <p className={` ${utilStyles.textDescriptions}`}>
+                  Let's create something meaningful (and beautiful){' '}
+                  <a
+                    className={`fw-bold text-decoration-none ${utilStyles.textDescriptions}`}
+                    href='#findme'
+                    rel='noopener noreferrer'
+                    alt='link to my contact page'
+                  >
+                    together
+                  </a>{' '}
+                  🌻
                 </p>
               </div>
             </div>
@@ -317,43 +334,37 @@ export default function Home() {
         </section>
 
         <section
-          className={`max-vh-100 pt-5 mt-5 ps-5 ms-2  ${utilStyles.page}`}
+          className={`max-vh-100 pt-5 mt-5 ms-2 ${utilStyles.page}`}
           id='findme'
           alt='find me contact page'
         >
           <div className='container pt-5 mt-5 mx-lg-5 mx-md-4'>
-            <div className='row'>
-              <div className='col-sm-4'>
-                <p className={utilStyles.headingXl}></p>
-              </div>
-              <div className={`col-sm-2`}>
-                <p className={utilStyles.headingXl}></p>
-              </div>
-              <div className={`col-sm-6  border-primary border-end text-end`}>
-                <h3 className={`me-5 ${utilStyles.headingXl}`}>Contact me</h3>
-              </div>
+            <div className={`row border-primary border-end text-end`}>
+              <h3 className={`me-5 text-end ${utilStyles.headingXl}`}>
+                Contact me
+              </h3>
             </div>
             <div className='row'>
               {' '}
               {/* row that contains 2 tables */}
-              <div className='col-xs-12 col-md-6'>
+              <div className='col-xs-12 col-md-7'>
                 {/*COL 1 for the email*/}
                 <div className='row '>
                   {' '}
                   {/* row that has name and hi*/}
-                  <div className='col-xs-12 col-sm-6 border-top border-end border-primary text-end py-3'>
+                  <div className='col-xs-12 col-sm-5 col-lg-7 border-top border-end border-primary text-end py-3'>
                     {/*1 COL for the name*/}
                     <div className=''>
                       <p className={`${utilStyles.headingLg}`}>
                         Paula Romero de Blas
                       </p>
-                      <p className={` ${utilStyles.headingMd}`}>/pawla/</p>
+                      <p className={`${utilStyles.headingMd}`}>/pawla/</p>
                       <p className={` ${utilStyles.textDescriptions}`}>
                         she/her/hers
                       </p>
                     </div>
                   </div>
-                  <div className='col-xs-12 col-sm-6 border-top border-end border-primary d-flex justify-content-center align-items-center py-5'>
+                  <div className='col-xs-12 col-sm-7 col-lg-5 border-top border-end border-primary d-flex justify-content-center align-items-center py-5'>
                     {' '}
                     {/*COL for the email*/}
                     <p
@@ -365,16 +376,18 @@ export default function Home() {
                 </div>
                 <div className='row border-top border-primary'>
                   {/*row with nothing and SM*/}
-                  <div className={`col-sm-6 border-end border-primary`}></div>
                   <div
-                    className={`col-sm-6 border-end border-primary text-end text-align-center`}
+                    className={`col-lg-7 col-sm-5 border-end border-primary`}
+                  ></div>
+                  <div
+                    className={`col-lg-5 col-sm-7 border-end border-primary text-end text-align-center`}
                   >
                     {' '}
                     <SMIcons />
                   </div>
                 </div>
               </div>
-              <div className='border-top border-primary-xs col-xs-12 col-md-6 ps-0 pe-5 pb-0 border-end border-bottom border-primary d-flex'>
+              <div className='border-top border-primary-xs col-xs-12 col-md-5 ps-0 pe-5 pb-0 border-end border-bottom border-primary d-flex'>
                 <Image
                   className='img-fluid'
                   src={ProfilePic}
