1a42b26e33752f06006547013a1ffac83d11c038
Paula <rdb.paula@gmail.com>
2022-11-23 17:55:17 +0700

    more meta test

--- components/layout.js
+++ components/layout.js
@@ -1,29 +1,27 @@
 import Head from 'next/head';
 // import { AnimatePresence } from 'framer-motion'
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 import styles from './layout.module.scss';
 import utilStyles from '../styles/utils.module.scss';
 import Link from 'next/link';
 import Header from './Header';
 
-
 const name = "Paula's Portfolio";
 export const siteTitle = "Paula's Portfolio";
 
 export default function Layout({ children, home }) {
-
   useEffect(() => {
     // To avoid FOUC
     if (
-        localStorage.theme === 'dark' ||
-        (!('theme' in localStorage) &&
-            window.matchMedia('(prefers-color-scheme: dark)').matches)
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
-        document.documentElement.classList.add('dark')
+      document.documentElement.classList.add('dark');
     } else {
-        document.documentElement.classList.remove('dark')
+      document.documentElement.classList.remove('dark');
     }
-})
+  });
 
   return (
     <div className='bg-secondary'>
@@ -31,18 +29,26 @@ export default function Layout({ children, home }) {
         <Head>
           <link rel='icon' href='/public/p_logo.ico' />
           <meta name='description' content='Portfolio ' />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'
+          />
           <meta
             property='og:image'
-            content={`https://og-image.vercel.app/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            content={`/nextjs-blog/public/p_logo.ico`}
           />
           <meta name='og:title' content={siteTitle} />
-          <meta name='twitter:card' /*en el futuro*/ content='summary_large_image' />
-        </Head>        
-       <Header/> 
-       {/* <AnimatePresence
+          <meta
+            name='twitter:card' /*en el futuro*/
+            content='summary_large_image'
+          />
+          <meta property='og:url' content='https://www.paulardb.design/' />
+          <meta property='og:type' content='website' />
+          <meta property='og:description' content='Check out my Portfolio' />
+          <meta property='og:image' content={'url of image'} />
+        </Head>
+        <Header />
+        {/* <AnimatePresence
                 exitBeforeEnter
                 initial={false}
                 onExitComplete={() => window.scrollTo(0, 0)}
@@ -57,15 +63,15 @@ export default function Layout({ children, home }) {
               <a>← Back to home</a>
             </Link>
             <h2 className={utilStyles.heading2XL}>
-                <Link href='/'>
-                  <a className={utilStyles.colorInherit}>{name}</a>
-                </Link>
-              </h2>
-              <h3 className={utilStyles.headingLg}>
-                <Link href='/'>
-                  <a className={utilStyles.colorInherit}>{name}</a>
-                </Link>
-              </h3>
+              <Link href='/'>
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+            <h3 className={utilStyles.headingLg}>
+              <Link href='/'>
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h3>
           </div>
         )}
       </div>
