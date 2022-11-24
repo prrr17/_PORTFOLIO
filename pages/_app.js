6ab880693546c18ac598c8379fce51d02911fa56
Paula <rdb.paula@gmail.com>
2022-11-24 08:43:04 +0700

    following arctype deployment blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
+import Head from 'next/head'
 import '@fontsource/italiana';
-import '@fontsource/abhaya-libre/400.css'
+import '@fontsource/abhaya-libre/400.css';
 import '../styles/global.scss';
 import '../styles/bootstrap-config.scss';
 import 'animate.css';
 
-
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-  }
+  return (
+    <>
+      <Head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang='eng'>
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta charSet='utf-8' />
       </Head>
       <body
